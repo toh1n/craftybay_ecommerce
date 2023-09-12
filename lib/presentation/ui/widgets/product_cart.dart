e216b688a87a61739f2ed9e32abf826e3803f4bb
tohin <tohin214@gmail.com>
2023-09-12 23:12:38 +0600

    init

---
+++ lib/presentation/ui/widgets/product_cart.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
