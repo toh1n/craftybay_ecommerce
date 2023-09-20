9d8e01f9b178521c7d2d5d153d7b3b226d0443ba
Tohin <tohin214@gmail.com>
2023-09-20 19:02:27 +0600

    added cart_screen.dart

--- lib/presentation/ui/screens/cart_screen.dart
+++ lib/presentation/ui/screens/cart_screen.dart
@@ -1,37 +1,91 @@
 import 'package:craftybay_ecommerce/presentation/state_holders/base_nav_controller.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:craftybay_ecommerce/presentation/ui/utility/app_colors.dart';
+import 'package:craftybay_ecommerce/presentation/ui/widgets/cart_product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
 
   @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        Get.find<BaseNavController>().backToHome();
-        return false;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text(
-            'Cart',
-            style: TextStyle(color: Colors.black),
-          ),
-          elevation: 0,
-          leading: IconButton(
-            onPressed: (){
-              Get.find<BaseNavController>().backToHome();
-            },
-            icon: const Icon(Icons.arrow_back_ios_new,color: Colors.black,),
-          ),
-          leadingWidth: 25,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Carts',
+          style: TextStyle(color: Colors.black),
         ),
-        body: const Center(
-          child: Text("Cart Screen"),
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Get.find<BaseNavController>().backToHome();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black54,
+          ),
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return const CartProductCard();
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            decoration: BoxDecoration(
+                color: AppColors.primaryColor.withOpacity(0.1),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                )),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Total Price',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                          color: Colors.black54),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '\$1000',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: AppColors.primaryColor),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 120,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Checkout'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
